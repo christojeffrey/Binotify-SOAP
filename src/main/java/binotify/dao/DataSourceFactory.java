bc55805aec53aa36461e039e7269a0c172325cf2
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-11-24 21:39:08 +0700

    change database credential to follow the one used on docker

--- src/main/java/binotify/dao/DataSourceFactory.java
+++ src/main/java/binotify/dao/DataSourceFactory.java
@@ -3,24 +3,31 @@ package binotify.dao;
 import java.sql.*;
 
 public class DataSourceFactory {
-    private static String HOST = "127.0.0.1";
+    
+    
+    
+    
+    private static String HOST = "binotify_soap_database";
     private static String PORT = "3306";
-    private static String DATABASE = "binotify_subscriptions";
-    private static String USER = "root";
-    private static String PASSWORD = "root";
+    private static String DATABASE = "tubes";
+    private static String USER = "tubes";
+    private static String PASSWORD = "tubes";
 
     private Connection conn = null;
-
+    
     private DataSourceFactory() {
         String url = String.format("jdbc:mysql://%s:%s/%s", HOST, PORT, DATABASE);
+        // print
+        System.out.println(url);
         try {
             this.conn = DriverManager.getConnection(url, USER, PASSWORD);
         } catch (SQLException throwable) {
             throwable.printStackTrace();
         }
     }
-
+    
     private static DataSourceFactory instance = new DataSourceFactory();
+
 
     public static DataSourceFactory getInstance() {
         return instance;
