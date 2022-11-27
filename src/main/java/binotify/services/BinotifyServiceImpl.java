173bdeb767d99c735d4ef1f4c56abb3886704d08
maspaitujaki <13520156@std.stei.itb.ac.id>
2022-11-27 15:33:46 +0700

    add logging

--- src/main/java/binotify/services/BinotifyServiceImpl.java
+++ src/main/java/binotify/services/BinotifyServiceImpl.java
@@ -1,34 +1,42 @@
 package binotify.services;
 
-import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import binotify.dao.LoggingImpl;
 import binotify.dao.SubscriptionImpl;
 import binotify.model.Respond;
+import com.sun.net.httpserver.HttpExchange;
 
 import java.net.InetSocketAddress;
+import java.net.URI;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.handler.MessageContext;
-import javax.xml.ws.spi.http.HttpExchange;
 
 @WebService(endpointInterface = "binotify.services.BinotifyService")
 public class BinotifyServiceImpl implements BinotifyService {
 
-    @Resource WebServiceContext context;
+    @Resource
+    WebServiceContext wsContext;
     
     // not a web method. used to get the IP address of the clients
-    public String getInfo(){
-        // still not working. need to search more on the internet. target: get the client's IP
-        MessageContext mc = context.getMessageContext();
+    public String getReqIP(){
+        MessageContext mc = wsContext.getMessageContext();
         HttpExchange exchange = (HttpExchange)mc.get("com.sun.xml.ws.http.exchange");
         InetSocketAddress remoteAddress = exchange.getRemoteAddress();
         String remoteHost = remoteAddress.getHostName();
-        return "Your IP address is: " + remoteHost + " and your port is: " + remoteAddress.getPort();
+        return remoteHost;
     }
+
+    public String getReqURI(){
+        MessageContext mc = wsContext.getMessageContext();
+        HttpExchange exchange = (HttpExchange)mc.get("com.sun.xml.ws.http.exchange");
+        String uri = String.valueOf(exchange.getRequestURI());
+        return uri;
+    }
+
+
 
     public Respond helloWorld() {
         return new Respond("Hello World");
@@ -40,6 +48,15 @@ public class BinotifyServiceImpl implements BinotifyService {
 
     public Respond newSubscription(String creator_id, String subscriber_id) {
         // todo: add logging
+        String IP = this.getReqIP();
+        String uri = this.getReqURI();
+        String description = new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName();
+        LoggingImpl.create(IP,description,uri);
+
+
 
         // create a new subscription
         try {
@@ -53,6 +70,14 @@ public class BinotifyServiceImpl implements BinotifyService {
 
     public Respond checkSubscription( String creator_id, String subscriber_id) {
         // todo: add logging
+        String IP = this.getReqIP();
+        String uri = this.getReqURI();
+        String description = new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName();
+        LoggingImpl.create(IP,description,uri);
+
 
         // get the status of a subscription
         try {
@@ -66,6 +91,14 @@ public class BinotifyServiceImpl implements BinotifyService {
 
     public Respond updateSubscription( String creator_id, String subscriber_id, String status) {
         // todo: add logging
+        String IP = this.getReqIP();
+        String uri = this.getReqURI();
+        String description = new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName();
+        LoggingImpl.create(IP,description,uri);
+
 
         // update the status of a subscription
         try {
